b78881b15936e74d49bdd4e2d46880e05decd5e6
Michael New <mikenew12@gmail.com>
2019-12-20 18:27:59 -0800

    Add minimal MobileSMS hook and skeleton app

---
+++ messagehook/.theos_old/obj/debug/armv7/Tweak.x.m
@@ -0,0 +1,155 @@
+#line 1 "Tweak.x"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#import <ChatKit/CKConversationList.h>
+
+
+#import <version.h>
+#import <os/log.h>
+
+@interface NSObject (Private)
+- (NSString*)_methodDescription;
+@end
+
+@interface IMMessage: NSObject {}
+@property (setter=_updateText:,nonatomic,retain) NSAttributedString * text; 
+@end
+
+@interface IMChat: NSObject {}
+@property (nonatomic,readonly) IMMessage * lastMessage;
+@end
+
+@interface CKConversation: NSObject {}
+-(void) splitLog:(NSString*)logString;
+-(void) setLocalUserIsTyping:(BOOL)isTyping;
+@property (retain, nonatomic) IMChat* chat;
+@end
+
+
+
+#include <substrate.h>
+#if defined(__clang__)
+#if __has_feature(objc_arc)
+#define _LOGOS_SELF_TYPE_NORMAL __unsafe_unretained
+#define _LOGOS_SELF_TYPE_INIT __attribute__((ns_consumed))
+#define _LOGOS_SELF_CONST const
+#define _LOGOS_RETURN_RETAINED __attribute__((ns_returns_retained))
+#else
+#define _LOGOS_SELF_TYPE_NORMAL
+#define _LOGOS_SELF_TYPE_INIT
+#define _LOGOS_SELF_CONST
+#define _LOGOS_RETURN_RETAINED
+#endif
+#else
+#define _LOGOS_SELF_TYPE_NORMAL
+#define _LOGOS_SELF_TYPE_INIT
+#define _LOGOS_SELF_CONST
+#define _LOGOS_RETURN_RETAINED
+#endif
+
+@class CKConversation; @class IMDaemonController; @class IMDaemon; 
+static void _logos_method$_ungrouped$CKConversation$splitLog$(_LOGOS_SELF_TYPE_NORMAL CKConversation* _LOGOS_SELF_CONST, SEL, NSString*); static void (*_logos_orig$_ungrouped$CKConversation$sendMessage$newComposition$)(_LOGOS_SELF_TYPE_NORMAL CKConversation* _LOGOS_SELF_CONST, SEL, id, BOOL); static void _logos_method$_ungrouped$CKConversation$sendMessage$newComposition$(_LOGOS_SELF_TYPE_NORMAL CKConversation* _LOGOS_SELF_CONST, SEL, id, BOOL); static void (*_logos_orig$_ungrouped$CKConversation$setLocalUserIsTyping$)(_LOGOS_SELF_TYPE_NORMAL CKConversation* _LOGOS_SELF_CONST, SEL, BOOL); static void _logos_method$_ungrouped$CKConversation$setLocalUserIsTyping$(_LOGOS_SELF_TYPE_NORMAL CKConversation* _LOGOS_SELF_CONST, SEL, BOOL); static BOOL (*_logos_orig$_ungrouped$IMDaemon$daemonInterface$shouldGrantAccessForPID$auditToken$portName$listenerConnection$setupInfo$setupResponse$)(_LOGOS_SELF_TYPE_NORMAL IMDaemon* _LOGOS_SELF_CONST, SEL, id, int, id, id, id, id, id*); static BOOL _logos_method$_ungrouped$IMDaemon$daemonInterface$shouldGrantAccessForPID$auditToken$portName$listenerConnection$setupInfo$setupResponse$(_LOGOS_SELF_TYPE_NORMAL IMDaemon* _LOGOS_SELF_CONST, SEL, id, int, id, id, id, id, id*); static BOOL (*_logos_orig$_ungrouped$IMDaemon$daemonInterface$shouldGrantPlugInAccessForPID$auditToken$portName$listenerConnection$setupInfo$setupResponse$)(_LOGOS_SELF_TYPE_NORMAL IMDaemon* _LOGOS_SELF_CONST, SEL, id, int, id, id, id, id, id*); static BOOL _logos_method$_ungrouped$IMDaemon$daemonInterface$shouldGrantPlugInAccessForPID$auditToken$portName$listenerConnection$setupInfo$setupResponse$(_LOGOS_SELF_TYPE_NORMAL IMDaemon* _LOGOS_SELF_CONST, SEL, id, int, id, id, id, id, id*); static BOOL (*_logos_orig$_ungrouped$IMDaemonController$setCapabilities$forListenerID$)(_LOGOS_SELF_TYPE_NORMAL IMDaemonController* _LOGOS_SELF_CONST, SEL, id, NSString *); static BOOL _logos_method$_ungrouped$IMDaemonController$setCapabilities$forListenerID$(_LOGOS_SELF_TYPE_NORMAL IMDaemonController* _LOGOS_SELF_CONST, SEL, id, NSString *); 
+
+#line 62 "Tweak.x"
+
+
+
+static void _logos_method$_ungrouped$CKConversation$splitLog$(_LOGOS_SELF_TYPE_NORMAL CKConversation* _LOGOS_SELF_CONST __unused self, SEL __unused _cmd, NSString* logString){
+    int stepLog = 800;
+    NSInteger strLen = [@([logString length]) integerValue];
+    NSInteger countInt = strLen / stepLog;
+
+    if (strLen > stepLog) {
+        for (int i=1; i <= countInt; i++) {
+            NSString *character = [logString substringWithRange:NSMakeRange((i*stepLog)-stepLog, stepLog)];
+            HBLogInfo(@"%@", character);
+        }
+        NSString *character = [logString substringWithRange:NSMakeRange((countInt*stepLog), strLen-(countInt*stepLog))];
+        HBLogInfo(@"%@", character);
+    } else {
+        HBLogInfo(@"%@", logString);
+    }
+}
+
+static void _logos_method$_ungrouped$CKConversation$sendMessage$newComposition$(_LOGOS_SELF_TYPE_NORMAL CKConversation* _LOGOS_SELF_CONST __unused self, SEL __unused _cmd, id arg1, BOOL arg2) {
+   _logos_orig$_ungrouped$CKConversation$sendMessage$newComposition$(self, _cmd, arg1, arg2);
+   NSLog(@"%@", self.chat.lastMessage.text);
+}
+
+static void _logos_method$_ungrouped$CKConversation$setLocalUserIsTyping$(_LOGOS_SELF_TYPE_NORMAL CKConversation* _LOGOS_SELF_CONST __unused self, SEL __unused _cmd, BOOL isTyping) {
+    _logos_orig$_ungrouped$CKConversation$setLocalUserIsTyping$(self, _cmd, isTyping);
+    NSLog(@"messagehook: User typing: %d", isTyping);
+    os_log(OS_LOG_DEFAULT, "messagehook: this is a log");
+}
+
+
+
+
+static BOOL _logos_method$_ungrouped$IMDaemon$daemonInterface$shouldGrantAccessForPID$auditToken$portName$listenerConnection$setupInfo$setupResponse$(_LOGOS_SELF_TYPE_NORMAL IMDaemon* _LOGOS_SELF_CONST __unused self, SEL __unused _cmd, id arg1, int arg2, id arg3, id arg4, id arg5, id arg6, id* arg7) {
+    NSLog(@"hooked IMDaemon");
+	return TRUE;
+}
+static BOOL _logos_method$_ungrouped$IMDaemon$daemonInterface$shouldGrantPlugInAccessForPID$auditToken$portName$listenerConnection$setupInfo$setupResponse$(_LOGOS_SELF_TYPE_NORMAL IMDaemon* _LOGOS_SELF_CONST __unused self, SEL __unused _cmd, id arg1, int arg2, id arg3, id arg4, id arg5, id arg6, id* arg7) {
+    NSLog(@"hooked IMDaemon");
+	return TRUE;
+}
+
+
+
+static BOOL _logos_method$_ungrouped$IMDaemonController$setCapabilities$forListenerID$(_LOGOS_SELF_TYPE_NORMAL IMDaemonController* _LOGOS_SELF_CONST __unused self, SEL __unused _cmd, id capabilities, NSString * listenerID) {
+    NSLog(@"hooked IMDaemonController");
+	NSLog(@"Capabilities: %@, lsitenerID: %@", capabilities, listenerID);
+	_logos_orig$_ungrouped$IMDaemonController$setCapabilities$forListenerID$(self, _cmd, capabilities, listenerID);
+	
+	return TRUE;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+static __attribute__((constructor)) void _logosLocalInit() {
+{Class _logos_class$_ungrouped$CKConversation = objc_getClass("CKConversation"); { char _typeEncoding[1024]; unsigned int i = 0; _typeEncoding[i] = 'v'; i += 1; _typeEncoding[i] = '@'; i += 1; _typeEncoding[i] = ':'; i += 1; memcpy(_typeEncoding + i, @encode(NSString*), strlen(@encode(NSString*))); i += strlen(@encode(NSString*)); _typeEncoding[i] = '\0'; class_addMethod(_logos_class$_ungrouped$CKConversation, @selector(splitLog:), (IMP)&_logos_method$_ungrouped$CKConversation$splitLog$, _typeEncoding); }MSHookMessageEx(_logos_class$_ungrouped$CKConversation, @selector(sendMessage:newComposition:), (IMP)&_logos_method$_ungrouped$CKConversation$sendMessage$newComposition$, (IMP*)&_logos_orig$_ungrouped$CKConversation$sendMessage$newComposition$);MSHookMessageEx(_logos_class$_ungrouped$CKConversation, @selector(setLocalUserIsTyping:), (IMP)&_logos_method$_ungrouped$CKConversation$setLocalUserIsTyping$, (IMP*)&_logos_orig$_ungrouped$CKConversation$setLocalUserIsTyping$);Class _logos_class$_ungrouped$IMDaemon = objc_getClass("IMDaemon"); MSHookMessageEx(_logos_class$_ungrouped$IMDaemon, @selector(daemonInterface:shouldGrantAccessForPID:auditToken:portName:listenerConnection:setupInfo:setupResponse:), (IMP)&_logos_method$_ungrouped$IMDaemon$daemonInterface$shouldGrantAccessForPID$auditToken$portName$listenerConnection$setupInfo$setupResponse$, (IMP*)&_logos_orig$_ungrouped$IMDaemon$daemonInterface$shouldGrantAccessForPID$auditToken$portName$listenerConnection$setupInfo$setupResponse$);MSHookMessageEx(_logos_class$_ungrouped$IMDaemon, @selector(daemonInterface:shouldGrantPlugInAccessForPID:auditToken:portName:listenerConnection:setupInfo:setupResponse:), (IMP)&_logos_method$_ungrouped$IMDaemon$daemonInterface$shouldGrantPlugInAccessForPID$auditToken$portName$listenerConnection$setupInfo$setupResponse$, (IMP*)&_logos_orig$_ungrouped$IMDaemon$daemonInterface$shouldGrantPlugInAccessForPID$auditToken$portName$listenerConnection$setupInfo$setupResponse$);Class _logos_class$_ungrouped$IMDaemonController = objc_getClass("IMDaemonController"); MSHookMessageEx(_logos_class$_ungrouped$IMDaemonController, @selector(setCapabilities:forListenerID:), (IMP)&_logos_method$_ungrouped$IMDaemonController$setCapabilities$forListenerID$, (IMP*)&_logos_orig$_ungrouped$IMDaemonController$setCapabilities$forListenerID$);} }
+#line 127 "Tweak.x"
---
+++ messagehook/Tweak.x
@@ -0,0 +1,125 @@
+/* How to Hook with Logos
+Hooks are written with syntax similar to that of an Objective-C @implementation.
+You don't need to #include <substrate.h>, it will be done automatically, as will
+the generation of a class list and an automatic constructor.
+
+%hook ClassName
+
+// Hooking a class method
++ (id)sharedInstance {
+	return %orig;
+}
+
+// Hooking an instance method with an argument.
+- (void)messageName:(int)argument {
+	%log; // Write a message about this call, including its class, name and arguments, to the system log.
+
+	%orig; // Call through to the original function with its original arguments.
+	%orig(nil); // Call through to the original function with a custom argument.
+
+	// If you use %orig(), you MUST supply all arguments (except for self and _cmd, the automatically generated ones.)
+}
+
+// Hooking an instance method with no arguments.
+- (id)noArguments {
+	%log;
+	id awesome = %orig;
+	[awesome doSomethingElse];
+
+	return awesome;
+}
+
+// Always make sure you clean up after yourself; Not doing so could have grave consequences!
+%end
+*/
+
+//#import <ChatKit/CKConversation.h>
+#import <ChatKit/CKConversationList.h>
+//#import <IMCore/IMChat.h>
+//#import <IMCore/IMHandle.h>
+#import <version.h>
+#import <os/log.h>
+
+@interface NSObject (Private)
+- (NSString*)_methodDescription;
+@end
+
+@interface IMMessage: NSObject {}
+@property (setter=_updateText:,nonatomic,retain) NSAttributedString * text; 
+@end
+
+@interface IMChat: NSObject {}
+@property (nonatomic,readonly) IMMessage * lastMessage;
+@end
+
+@interface CKConversation: NSObject {}
+-(void) splitLog:(NSString*)logString;
+-(void) setLocalUserIsTyping:(BOOL)isTyping;
+@property (retain, nonatomic) IMChat* chat;
+@end
+
+
+%hook CKConversation
+
+%new
+-(void) splitLog:(NSString*)logString{
+    int stepLog = 800;
+    NSInteger strLen = [@([logString length]) integerValue];
+    NSInteger countInt = strLen / stepLog;
+
+    if (strLen > stepLog) {
+        for (int i=1; i <= countInt; i++) {
+            NSString *character = [logString substringWithRange:NSMakeRange((i*stepLog)-stepLog, stepLog)];
+            HBLogInfo(@"%@", character);
+        }
+        NSString *character = [logString substringWithRange:NSMakeRange((countInt*stepLog), strLen-(countInt*stepLog))];
+        HBLogInfo(@"%@", character);
+    } else {
+        HBLogInfo(@"%@", logString);
+    }
+}
+
+-(void)sendMessage:(id)arg1 newComposition:(BOOL)arg2 {
+   %orig;
+   NSLog(@"msghk: %@", self.chat.lastMessage.text);
+}
+
+- (void)setLocalUserIsTyping:(BOOL)isTyping {
+    %orig;
+    NSLog(@"msghk: User typing: %d", isTyping);
+}
+
+%end
+
+%hook IMDaemon
+-(BOOL)daemonInterface:(id)arg1 shouldGrantAccessForPID:(int)arg2 auditToken:(id)arg3 portName:(id)arg4 listenerConnection:(id)arg5 setupInfo:(id)arg6 setupResponse:(id*)arg7 {
+    NSLog(@"msghk: hooked IMDaemon");
+	return TRUE;
+}
+-(BOOL)daemonInterface:(id)arg1 shouldGrantPlugInAccessForPID:(int)arg2 auditToken:(id)arg3 portName:(id)arg4 listenerConnection:(id)arg5 setupInfo:(id)arg6 setupResponse:(id*)arg7 {
+    NSLog(@"msghk: hooked IMDaemon");
+	return TRUE;
+}
+%end
+
+%hook IMDaemonController
+- (BOOL)setCapabilities:(id)capabilities forListenerID:(NSString *)listenerID {
+    NSLog(@"hooked IMDaemonController");
+	NSLog(@"msghk: Capabilities: %@, lsitenerID: %@", capabilities, listenerID);
+	%orig;
+	//%orig(Chats, listenerID);
+	return TRUE;
+
+}
+%end
+
+
+//%hook CKConversationListController
+//-(void)setConversationList:(CKConversationList *)arg1 {
+//    %orig;
+//    //HBLogInfo(@"CKConversationListController list %@", arg1.conversations.description);
+//    NSArray *convos = MSHookIvar<NSArray *>(arg1, "conversations");
+//    HBLogInfo(@"CKConversationListController list %@", convos.description);
+//
+//}
+//%end
