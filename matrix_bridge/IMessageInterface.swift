1e2be1dfcad8102ed061e0a18bb31c65ef1f6431
Michael New <mikenew12@gmail.com>
2020-02-27 23:02:41 -0800

    Start moving imessage methods into a single file

---
+++ matrix_bridge/IMessageInterface.swift
@@ -0,0 +1,29 @@
+import Foundation
+import SwiftMatrixSDK
+
+struct IMessage {
+    let message: String
+    let guid: String
+    let recipientName: String
+}
+
+class IMessageInterface {
+
+    var messageRecieved: ((IMessage) -> Void)?
+
+    init() {
+        let center = NSDistributedNotificationCenter.default!
+        let mainQueue = OperationQueue.main
+        center.addObserver(forName: NSNotification.Name("messagehook"), object: nil, queue: mainQueue) { (note) in
+            log("got notification")
+            let message = note.userInfo?["message"] as? String ?? "no message"
+            let guid = note.userInfo?["guid"] as? String ?? "guid not found"
+            let name = note.userInfo?["recipientName"] as? String ?? "recipient unkown"
+            let i = IMessage(message: message, guid: guid, recipientName: name)
+            self.messageRecieved?(i)
+
+            log(message + " (" + name + ")")
+            //self.matrixHandler?.send(message: "\(name): " + message + " (guid: \(guid))")
+        }
+    }
+}
